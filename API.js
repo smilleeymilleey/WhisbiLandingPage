e76f4b0725dadcf8636eb116fc95c7bdd2833197
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-13 12:46:14 -0500

    hit one api endpoint onclick

--- API.js
+++
@@ -1,8 +0,0 @@
-function getRates () {
-    console.log("its working");
-  }
-  
-  let oReq = new XMLHttpRequest();
-  oReq.addEventListener("click", getRates);
-  oReq.open("GET", "http://api.exchangeratesapi.io/v1/KEY/convert");
-  oReq.send();
