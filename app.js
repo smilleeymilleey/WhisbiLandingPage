2f8abd57e01bf7266afd5b024192a75e4745e915
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-14 18:49:27 -0500

    looped through currency array and dynamically created drop down element options

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+let currencies = [ "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL" ];
 
+
+
+currencies.forEach(element => {
+  let option = document.createElement("option")
+  option.textContent = element;
+  document.getElementById("newCurrency").appendChild(option)
+  
+});
+
+
+    
+ 
+  
+  
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -16,6 +31,7 @@ function getRates() {
   let newCurrency = endSelect.options[endSelect.selectedIndex].text;
   let startCurrency= startSelect.options[startSelect.selectedIndex].text;
   let startAmount = document.getElementById("startAmount").value
+
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -35,8 +51,14 @@ function getRates() {
         document.getElementById("convertedAmount").innerHTML= newAmount + " " + newCurrency;
         document.getElementById("date").innerHTML= "Last updated " + res.date;
         document.getElementById("time").innerHTML = formattedTime;
+     
     } 
       
+    
+
+
+
+    // inside of the loop for each currency create option element and append child to newcurrencyselect 
       
 
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const fs = require('fs');
 var static = require('node-static');
 
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
