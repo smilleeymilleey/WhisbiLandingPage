0c2faeaa48d0565badc5f4574b28725d3059a465
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-14 12:07:40 -0500

    display relevant conversion information

--- app.js
+++ app.js
@@ -9,39 +9,27 @@ function getRates() {
   let startCurrency= startSelect.options[startSelect.selectedIndex].text;
   let startAmount = document.getElementById("startAmount").value
 
-  console.log(startCurrency, newCurrency)
-    console.log("its working");
-    console.log(startAmount)
-    
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.responseText)
         let conversionRate = res.rates[newCurrency]
-
-        console.log(conversionRate)
-        let newAmount = startAmount*conversionRate 
-        
-        document.getElementById("conversionRate").innerHTML=res.rates[newCurrency]
-        document.getElementById("convertedAmount").innerHTML= newAmount;
-
-
-        
-        console.log(newAmount)
-      } 
+        let newAmount = startAmount*conversionRate
       
-      // manually calculate exchange rates 
+        console.log(res)
 
-   
-       
-
-   
+        document.getElementById("start").innerHTML=startAmount + " Euros =";
+        document.getElementById("conversionRate").innerHTML=res.rates[newCurrency] + " " + newCurrency;
+        document.getElementById("convertedAmount").innerHTML= newAmount + " " + newCurrency;
+    } 
+      
+      
 
     };
 
     // currency converter is not free so find end point that gives conversion rates and manually convert rates for drop down menu items 
     xhttp.open("GET", "http://api.exchangeratesapi.io/v1/latest?access_key=04f180122fa4b611c6851b8f7b6774cd&base=" + startCurrency + 
-  "&symbols=" + newCurrency);
+  "&symbols=" + " " + newCurrency);
     xhttp.send();
 
 }
