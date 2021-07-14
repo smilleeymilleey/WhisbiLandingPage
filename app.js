e6fe865f89611d0a340609533cf3fb02131ed0e5
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-13 20:23:03 -0500

    added correct end point using dropdown variable

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
 // determine rates 
 
-let startSelect= document.getElementById("names")
 // let startCurrency = startSelect.options[startSelect.selectedIndex].value
-document.getElementById("convertButton").onclick = getRates();
-
+document.getElementById("convertButton").onclick =getRates();
 
 
 
 function getRates () {
+  let startSelect= document.getElementById("names")
+  let currency= startSelect.options[startSelect.selectedIndex].text;
+  console.log(currency)
     console.log("its working");
+    
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -19,8 +21,7 @@ function getRates () {
     };
 
     // currency converter is not free so find end point that gives conversion rates and manually convert rates for drop down menu items 
-    xhttp.open("GET", "http://api.exchangeratesapi.io/v1/latest?access_key=04f180122fa4b611c6851b8f7b6774cd");
+    xhttp.open("GET", "http://api.exchangeratesapi.io/v1/latest?access_key=04f180122fa4b611c6851b8f7b6774cd&base=" + currency);
     xhttp.send();
-
 
 }
