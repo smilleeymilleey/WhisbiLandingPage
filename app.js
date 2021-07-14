aec4630bd3578cd631cb45a1e194c60b74251a7a
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-13 21:56:56 -0500

    if I mess up just use this commit to start over haha

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 // determine rates 
 
 // let startCurrency = startSelect.options[startSelect.selectedIndex].value
-document.getElementById("convertButton").onclick =getRates();
+document.getElementById("convertButton").onclick=getRates();
 
 
 
 function getRates () {
   let startSelect= document.getElementById("names")
-  let endSelect= document.getElementById("")
+  let endSelect= document.getElementById("newCurrency")
+  let newCurrency = endSelect.options[endSelect.selectedIndex].text;
   let currency= startSelect.options[startSelect.selectedIndex].text;
-  
+
   console.log(currency)
     console.log("its working");
     
@@ -19,12 +20,14 @@ function getRates () {
       if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.responseText)
         document.getElementById("display").innerHTML= res.date
+        
        
       } 
     };
 
     // currency converter is not free so find end point that gives conversion rates and manually convert rates for drop down menu items 
-    xhttp.open("GET", "http://api.exchangeratesapi.io/v1/latest?access_key=04f180122fa4b611c6851b8f7b6774cd&base=" + currency);
+    xhttp.open("GET", "http://api.exchangeratesapi.io/v1/latest?access_key=04f180122fa4b611c6851b8f7b6774cd&base=" + currency + 
+  "&symbols=" + newCurrency);
     xhttp.send();
 
 }
