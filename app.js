062a80d69ec06f3741a1cfb8f896b3b8c6c4cf78
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-13 23:05:44 -0500

    converted from euros to other and displayed

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 
 // determine rates 
 
-// let startCurrency = startSelect.options[startSelect.selectedIndex].value
-// document.getElementById("convertButton").onclick=getRates();
-
-
 
 function getRates () {
   let startSelect= document.getElementById("names")
   let endSelect= document.getElementById("newCurrency")
   let newCurrency = endSelect.options[endSelect.selectedIndex].text;
   let startCurrency= startSelect.options[startSelect.selectedIndex].text;
+  let startAmount = document.getElementById("startAmount").value
 
   console.log(startCurrency, newCurrency)
     console.log("its working");
+    console.log(startAmount)
     
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.responseText)
+        let conversionRate = res.rates[newCurrency]
+        let newAmount = startAmount*conversionRate 
         
         document.getElementById("exchangeRate").innerHTML=res.rates[newCurrency]
-       
+        document.getElementById("convertedAmount").innerHTML=newAmount;
+
+
+        
+        console.log(newAmount)
       } 
+      
+      // manually calculate exchange rates 
+
+   
+       
+
+   
+
     };
 
     // currency converter is not free so find end point that gives conversion rates and manually convert rates for drop down menu items 
