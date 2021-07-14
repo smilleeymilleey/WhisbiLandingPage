a951efc4dc646a6552451f82b67855482029184a
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-14 15:49:56 -0500

    added navbar styling

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
-// determine rates 
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 function getRates() {
   let startSelect= document.getElementById("names")
