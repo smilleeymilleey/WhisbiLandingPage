2b56c4ed000df5d4402b4f511ceec9fd384c1cd2
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-14 23:07:25 -0500

    added best practices SEO tags to HTML

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 // chart begin 
 
 window.onload = function() {
-
   var dataPoints = [];
-  
+
   var chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
     theme: "light2",
@@ -25,8 +24,8 @@ window.onload = function() {
       dataPoints: dataPoints
     }]
   });
-  
-  window.addData = function (data) {
+  // retrieves data from parse data function, loops through data, and pushes new data to new array and displays parsed data on specified data points 
+  window.addData = function(data) {
     console.log(data)
     for (var i = 0; i < data.length; i++) {
       dataPoints.push({
@@ -36,18 +35,11 @@ window.onload = function() {
     }
     console.log(dataPoints);
     chart.render();
-  
   }
 }
-
-  
-  
-
 // chart end
 
 let currencies = [ "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL" ];
-
-
 
 currencies.forEach(element => {
   let option = document.createElement("option")
@@ -56,12 +48,11 @@ currencies.forEach(element => {
   
 });
 
+// converts api results to the format that the chart library is expecting and sends info to addData function
 function parseChartData(data){
   let parsedData = Object.keys(data.rates).map(item => { return {"currency": item, "rate": data.rates[item]} });
-  
   window.addData(parsedData);
 }
-
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleNav() {
@@ -73,13 +64,13 @@ function toggleNav() {
   }
 }
 
+// displays and converts currency conversions 
 function getRates() {
   let startSelect= document.getElementById("names")
   let endSelect= document.getElementById("newCurrency")
   let newCurrency = endSelect.options[endSelect.selectedIndex].text;
   let startCurrency= startSelect.options[startSelect.selectedIndex].text;
   let startAmount = document.getElementById("startAmount").value
-
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -89,15 +80,11 @@ function getRates() {
         let newAmount = startAmount*conversionRate
         let unix_timestamp = res.timestamp;
         let date = new Date(unix_timestamp * 1000);
-        let hours = date.getHours();
-        let minutes = "0" + date.getMinutes();
-        let seconds = "0" + date.getSeconds();
-        let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
+      
         document.getElementById("start").innerHTML=startAmount + " Euros =";
         document.getElementById("conversionRate").innerHTML= "1 EUR = " + res.rates[newCurrency] + " " + newCurrency;
         document.getElementById("convertedAmount").innerHTML= newAmount + " " + newCurrency;
-        document.getElementById("date").innerHTML= "Last updated " + res.date;
+        document.getElementById("date").innerHTML= "Last updated " + date;
      
         parseChartData(res);
     } 
