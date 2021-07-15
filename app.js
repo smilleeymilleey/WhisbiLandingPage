d8d0468c9110637c47d31352b3c977f94a316c00
Caitlyn <caitlynmiley.cm@gmail.com>
2021-07-14 20:37:05 -0500

    updated dropdown menu and relocated button

--- app.js
+++ app.js
@@ -10,20 +10,16 @@ currencies.forEach(element => {
 });
 
 
-    
- 
-  
-  
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function toggleNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function toggleNav() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
 
 function getRates() {
   let startSelect= document.getElementById("names")
